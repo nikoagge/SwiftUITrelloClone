f3dd8346b47b996af630519b50ce61b4b30dc874
nikoagge <nikoagge@gmail.com>
2023-07-18 19:28:48 +0300

    Implementation for exported type identifiers

--- SwiftUITrelloClone/Models/BoardList.swift
+++ SwiftUITrelloClone/Models/BoardList.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class BoardList: ObservableObject, Identifiable {
+class BoardList: NSObject, ObservableObject, Identifiable, Codable {
     private(set) var id = UUID()
     var boardId: UUID
     @Published var name: String
@@ -21,5 +21,61 @@ class BoardList: ObservableObject, Identifiable {
         self.boardId = boardId
         self.cards = cards
         self.name = name
+        
+        super.init()
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.boardId = try container.decode(UUID.self, forKey: .boardId)
+        self.cards = try container.decode([Card].self, forKey: .cards)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        
+        super.init()
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(boardId, forKey: .boardId)
+        try container.encode(cards, forKey: .cards)
+        try container.encode(id, forKey: .id)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case boardId, cards, id, name
+    }
+}
+
+// MARK: - NSItemProviderWriting
+
+extension BoardList: NSItemProviderWriting {
+    static let typeIdentifier = "com.napps.SwiftUITrelloClone.BoardList"
+    static var writableTypeIdentifiersForItemProvider: [String] {
+        [typeIdentifier]
+    }
+    
+    func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+        do {
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.outputFormatting = .prettyPrinted
+            completionHandler(try jsonEncoder.encode(self), nil)
+        } catch {
+            completionHandler(nil, error)
+        }
+        
+        return nil
+    }
+}
+
+// MARK: - NSItemProviderReading
+
+extension BoardList: NSItemProviderReading {
+    static var readableTypeIdentifiersForItemProvider: [String] {
+        [typeIdentifier]
+    }
+    
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Self {
+        try JSONDecoder().decode(Self.self, from: data)
     }
 }
--- SwiftUITrelloClone/Models/Card.swift
+++ SwiftUITrelloClone/Models/Card.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Card: ObservableObject, Identifiable {
+class Card: NSObject, ObservableObject, Identifiable, Codable {
     private(set) var id = UUID()
     var boardListId: UUID
     @Published var content: String
@@ -18,5 +18,60 @@ class Card: ObservableObject, Identifiable {
     ) {
         self.boardListId = boardListId
         self.content = content
+        
+        super.init()
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.boardListId = try container.decode(UUID.self, forKey: .boardListId)
+        self.content = try container.decode(String.self, forKey: .content)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        
+        super.init()
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(boardListId, forKey: .boardListId)
+        try container.encode(content, forKey: .content)
+        try container.encode(id, forKey: .id)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case boardListId, content, id
+    }
+}
+
+// MARK: - NSItemProviderWriting
+
+extension Card: NSItemProviderWriting {
+    static let typeIdentifier = "com.napps.SwiftUITrelloClone.Card"
+    static var writableTypeIdentifiersForItemProvider: [String] {
+        [typeIdentifier]
+    }
+    
+    func loadData(withTypeIdentifier typeIdentifier: String, forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+        do {
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.outputFormatting = .prettyPrinted
+            completionHandler(try jsonEncoder.encode(self), nil)
+        } catch {
+            completionHandler(nil, error)
+        }
+        
+        return nil
+    }
+}
+
+// MARK: - NSItemProviderReading
+
+extension Card: NSItemProviderReading {
+    static var readableTypeIdentifiersForItemProvider: [String] {
+        [typeIdentifier]
+    }
+    
+    static func object(withItemProviderData data: Data, typeIdentifier: String) throws -> Self {
+        try JSONDecoder().decode(Self.self, from: data)
     }
 }
