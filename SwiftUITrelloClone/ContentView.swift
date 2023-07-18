aaa53a06464b7b9238dcaae5c8c760ce06f0a4e1
nikoagge <nikoagge@gmail.com>
2023-07-18 16:52:13 +0300

    Initial Commit

---
+++ SwiftUITrelloClone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUITrelloClone
+//
+//  Created by Nikos Aggelidis on 18/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITrelloClone/SwiftUITrelloCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrelloCloneApp.swift
+//  SwiftUITrelloClone
+//
+//  Created by Nikos Aggelidis on 18/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrelloCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
