7d07283005e16096d35ac464741e2bc986d827d9
nikoagge <nikoagge@gmail.com>
2023-07-19 17:48:52 +0300

    Created BoardListView

---
+++ SwiftUITrelloClone/Views/BoardListView.swift
@@ -0,0 +1,96 @@
+//
+//  BoardListView.swift
+//  SwiftUITrelloClone
+//
+//  Created by Nikos Aggelidis on 19/7/23.
+//
+
+import SwiftUI
+
+struct BoardListView: View {
+    @ObservedObject var board: Board
+    @StateObject var boardList: BoardList
+    
+    var body: some View {
+        VStack(
+            alignment: .leading,
+            spacing: 16
+        ) {
+            headerView
+            
+            listView
+                .listStyle(.plain)
+            
+            Button("+ Add Card") {
+                
+            }
+            .padding(.horizontal)
+            .frame(maxWidth: .infinity)
+        }
+        .padding(.vertical)
+        .background(Color.boardListBackgroundColor)
+        .frame(width: 310)
+        .cornerRadius(8)
+        .foregroundColor(.black)
+    }
+    
+    private var headerView: some View {
+        HStack(alignment: .top) {
+            Text(boardList.name)
+                .font(.headline)
+                .lineLimit(2)
+            
+            Spacer()
+            
+            Menu {
+                Button("Rename") {
+                    
+                }
+                
+                Button(
+                    "Delete",
+                    role: .destructive
+                ) {
+                    
+                }
+            } label: {
+                Image(systemName: "ellipsis.circle")
+                    .imageScale(.large)
+            }
+        }
+        .padding(.horizontal)
+    }
+    
+    private var listView: some View {
+        List {
+            ForEach(boardList.cards) { card in
+                Text(card.content)
+            }
+            .listRowSeparator(.hidden)
+            .listRowInsets(.init(
+                top: 4,
+                leading: 8,
+                bottom: 4,
+                trailing: 8
+            ))
+            .listRowBackground(Color.clear)
+        }
+
+    }
+}
+
+struct BoardListView_Previews: PreviewProvider {
+    @StateObject static var board = Board.stub
+    
+    static var previews: some View {
+        BoardListView(
+            board: board,
+            boardList: board.boardLists[0]
+        )
+        .previewLayout(.sizeThatFits)
+        .frame(
+            width: 310,
+            height: 530
+        )
+    }
+}
